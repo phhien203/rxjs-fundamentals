a12b14a4a9d33b0a6d542a26305aeb70cd080c8a
Pham Huu Hien <phhien203@gmail.com>
2022-08-14 20:13:34 +0700

    feat: basic counter

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,5 +1,15 @@
-import { fromEvent, interval, merge, NEVER } from 'rxjs';
+import { fromEvent, interval, merge, NEVER, timer } from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+const interval$ = interval(1000);
+let sub;
+
+start$.subscribe(() => {
+  sub = interval$.subscribe(setCount);
+});
+
+pause$.subscribe(() => {
+  sub && sub.unsubscribe();
+});
